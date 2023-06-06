e908cd0a8f3155a3d0ee085b4ce82435fe5b90b5
fsousac <felipescoelho517@gmail.com>
2023-06-05 21:16:13 -0300

    reorganização no intellij

--- src/br.com.dio.desafio.dominio/Curso.java
+++
@@ -1,7 +0,0 @@
-package br.com.dio.desafio.dominio;
-
-public class Curso {
-    String titulo;
-    String descricao;
-    int cargaHoraria;
-}
--- src/br.com.dio.desafio.dominio/Mentoria.java
+++
@@ -1,6 +0,0 @@
-package br.com.dio.desafio.dominio;
-
-public class Mentoria {
-    private String titulo, descricao;
-    private LocalDate data;
-}
