5c538dabacff9482c6955aa4be830b69d01cc008
fsousac <felipescoelho517@gmail.com>
2023-06-16 18:29:34 -0300

    Create Mentoria e Curso

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -28,4 +28,13 @@ public class Mentoria {
     public void setData(LocalDate data) {
         this.data = data;
     }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", data=" + data +
+                '}';
+    }
 }
