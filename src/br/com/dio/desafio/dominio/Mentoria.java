d6e9b565f38a369698061fbc46380475353d9362
fsousac <felipescoelho517@gmail.com>
2023-06-18 22:45:38 -0300

    Criação das classes Bootcamp e Dev

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -1,25 +1,8 @@
 package src.br.com.dio.desafio.dominio;
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo, descricao;
+public class Mentoria extends Conteudo{
     private LocalDate data;
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public LocalDate getData() {
         return data;
@@ -32,9 +15,14 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20;
     }
 }
